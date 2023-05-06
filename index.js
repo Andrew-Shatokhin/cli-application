3fee3de9e4b81d7ec6507c9df0d0ef1fb929374c
Andrew <shatokhin.a.p@gmail.com>
2023-05-06 21:16:20 +0300

    Add commander and README

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
+const { Command } = require("commander");
 const contacts = require("./contacts");
-console.log(contacts)
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-      case "list":
-          const allContacts = await contacts.listContacts();
-          return console.log(allContacts);
-    //   break;
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+    
 
     case "get":
-          const oneContact = await contacts.getContactById(id);
-           return console.log(oneContact);
-    //   break;
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+    
 
     case "add":
-          const newContact = await contacts.addContact({ name, email, phone });
-          return console.log(newContact);
-    //   break;
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+    
 
     case "remove":
-          const removeContact = await contacts.removeContact(id);
-          return console.log(removeContact);
-    //   break;
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
+    
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "drsAJ4SHPYqZeG-83QTVW" });
-// invokeAction({
-//   action: "add",
-//   name: "Mishanya",
-//   email: "Mishanya@gmail.com",
-//   phone: "(111) 111-1111",
-// });
-invokeAction({
-  action: "remove",
-  id: "kFZcqr3fY1Nonc05Qlt20",
-  });
+invokeAction(argv);
